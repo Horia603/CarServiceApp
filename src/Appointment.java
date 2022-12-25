9c7307f0bf846947c6fc5642e946f9b46ffbd72a
Horia <senduchoria@yahoo.com>
2022-12-25 03:29:08 +0200

    Appointments Vector Created

---
+++ src/Appointment.java
@@ -0,0 +1,7 @@
+public class Appointment {
+    String start_day;
+    String start_hour;
+    String finish_day;
+    String finish_hour;
+    int user_id;
+}
--- src/MainPageForm.java
+++ src/MainPageForm.java
@@ -8,6 +8,8 @@ import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.*;
+import java.util.Vector;
 
 public class MainPageForm extends JDialog{
     private JTable WeekTable;
@@ -44,17 +46,60 @@ public class MainPageForm extends JDialog{
         centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
         WeekTable.setCellSelectionEnabled(true);
 
-        for(int i=0;i<12;i++)
+        for(int i=0;i<24;i++)
         {
-            String ora=Integer.toString(i+8)+":00 - " + Integer.toString(i+9)+":00";
+            String ora;
+            if(i%2==0)
+            {
+                ora=Integer.toString(i+8-i/2)+":00 - " + Integer.toString(i+8-i/2)+":30";
+            }
+            else
+            {
+                ora=Integer.toString(i+7-i/2)+":30 - " + Integer.toString(i+8-i/2)+":00";
+            }
             String row[]={ora,ora,ora,ora,ora};
             dtm.addRow(row);
         }
 
+        Vector<Appointment> appointments = new Vector<Appointment>();
+
+        final String DB_URL = "jdbc:mysql://127.0.0.1:3306/sys";
+        final String USERNAME = "root";
+        final String PASSWORD = "Horia1975";
+        try {
+            Appointment appointment=null;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM appointments";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                appointment = new Appointment();
+                appointment.start_day = resultSet.getString("start_day");
+                appointment.start_hour = resultSet.getString("start_hour");
+                appointment.finish_day = resultSet.getString("finish_day");
+                appointment.finish_hour = resultSet.getString("finish_hour");
+                appointment.user_id = Integer.parseInt(resultSet.getString("user_id"));
+                appointments.add(appointment);
+            }
+            stmt.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        for(int i=0;i<appointments.size();i++)
+        {
+
+        }
         setVisible(true);
         WeekTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+
                 super.mouseClicked(e);
             }
         });
